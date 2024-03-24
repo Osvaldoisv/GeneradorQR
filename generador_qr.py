16a6f1573b9576f6bc87ede94916c3cfa9f9c164
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-03-24 20:20:56 -0300

    qr funcionalidad lista

--- generador_qr.py
+++ generador_qr.py
@@ -6,5 +6,25 @@ filename = "qr_codes/qr_code.png"
 def generate_qr_code(url, filename):
     qr = qrcode.QRCode(
         version=1,
-        error_correction=
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
     )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", black_color="white")
+    img.save(filename)
+
+# creamos app web con streamlit
+st.set_page_config(page_title="QR Code Generator", page_icon="/", layout="centered")
+st.image("images/supports.png", use_column_width=True)
+st.title("QR Code Generator")
+url = st.text_input("Enter the URL")
+
+if st.button("Generate QR Code"):
+    generate_qr_code(url, filename)
+    st.image(filename, use_column_width=True)
+    with open(filename, "rb") as f:
+        image_data = f.read()
+    download = st.download_button(label="Download QR", data=image_data, file_name="qr_generated.png")
